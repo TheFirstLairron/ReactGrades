ebdb3abc80ef6ce1b6e07976d11aa6ca207b43f5
Zachary Allnutt <TheFirstLairron@users.noreply.github.com>
2017-08-02 09:17:02 -0700

    Update gradesCreation.sql

--- SQLScripts/gradesCreation.sql
+++ SQLScripts/gradesCreation.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Grades]
 (
-	[StudentID] INT NOT NULL PRIMARY KEY, 
+	[ID] INT NOT NULL PRIMARY KEY, 
     [ClassID] INT NOT NULL, 
     [GPA] INT NOT NULL DEFAULT 0, 
     CONSTRAINT [ClassFK] FOREIGN KEY ([ClassID]) REFERENCES [Courses]([CourseID]), 
